c09e5a8682e4c4b13005d648764d3de9431b94e5
Sendeke, Kidus <kiduss@uw.edu>
2019-02-28 11:50:34 -0800

    initial commit

---
+++ clients/ahod/constants/routes.js
@@ -0,0 +1,12 @@
+export const routes = {
+    SIGN_IN = '/signIn', 
+    SING_UP = '/singUp',
+    HOME = '/home',
+    GROUPS = '/groups',
+    GROUP = '/groups/:id',
+    CREATE_GROUP = '/create_group',
+    UPDATE_PROFILE = '/update_prof', 
+    PROFILE = '/profile',
+    ACCOUNT = '/account', 
+    ABOUT = '/about'
+}
