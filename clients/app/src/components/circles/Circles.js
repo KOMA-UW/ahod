3b468a6fb0aa6778459f69c41d7c19b0c5192db8
Muatasim Qazi <muatasimqazi@gmail.com>
2019-05-10 03:05:27 -0700

    added circle

---
+++ clients/app/src/components/circles/Circles.js
@@ -0,0 +1,145 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Circle from './Circle';
+import classNames from 'classnames';
+import aMember from '../../img/group_1.jpg';
+import { Container, Row, Col } from 'react-grid-system';
+import { Typography, Divider } from '@material-ui/core';
+
+const members = [
+  {
+    name: 'John',
+    photoUrl: 'https://material-ui.com/static/images/avatar/1.jpg',
+    details: 'Some details'
+  },
+  {
+    name: 'Sam',
+    photoUrl: 'https://material-ui.com/static/images/avatar/2.jpg'
+  },
+  {
+    name: 'Tom',
+    photoUrl: 'https://material-ui.com/static/images/avatar/3.jpg'
+  },
+  {
+    name: 'Wendy',
+    photoUrl: 'https://material-ui.com/static/images/avatar/4.jpg'
+  },
+  {
+    name: 'Melody',
+    photoUrl: 'https://material-ui.com/static/images/avatar/5.jpg'
+  },
+  {
+    name: 'Stuart',
+    photoUrl: 'https://material-ui.com/static/images/avatar/6.jpg'
+  },
+  {
+    name: 'Anita',
+    photoUrl: 'https://material-ui.com/static/images/avatar/7.jpg'
+  },
+  {
+    name: 'Mary',
+    photoUrl: 'https://material-ui.com/static/images/avatar/2.jpg'
+  },
+  {
+    name: 'Nina',
+    photoUrl: 'https://material-ui.com/static/images/avatar/6.jpg'
+  },
+  {
+    name: 'Ada',
+    photoUrl: aMember
+  }
+];
+
+const styles = {
+  root: {
+    position: 'relative',
+    width: 240,
+    height: 240,
+    // border: '2px solid black',
+    borderRadius: '150px',
+    margin: 150,
+    backgroundColor: '#cfc8c16b'
+    // backgroundImage: `url(${aMember})`,
+    // backgroundRepeat: 'no-repeat',
+    // backgroundSize: 'auto',
+    // backgroundPosition: '50% 50%',
+    // bbackgroundClip: 'padding-box'
+  },
+  text: {
+    padding: 20
+  }
+};
+
+const mainCircle = 360 / members.length;
+const radius = 190;
+const offsetToParentCenter = 240 / 2.0;
+const offsetToChildren = 30;
+
+const totalOffset = offsetToParentCenter - offsetToChildren;
+class CircleView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    };
+    this.handleHover = this.handleHover.bind(this);
+  }
+
+  handleHover(event) {
+    console.log(event);
+    this.setState({
+      text: event
+    });
+  }
+
+  render() {
+    console.log(this);
+    const { classes } = this.props;
+
+    const cliqueMembers = members.map((member, i) => {
+      const y = Math.sin(mainCircle * i * (Math.PI / 180)) * radius;
+      const x = Math.cos(mainCircle * i * (Math.PI / 180)) * radius;
+      const top = (y + totalOffset).toString() + 'px';
+      const left = (x + totalOffset).toString() + 'px';
+
+      const item = (
+        <Circle
+          key={i}
+          image={member.photoUrl}
+          top={top}
+          left={left}
+          handleImageHover={this.handleHover}
+          text={member.name}
+        />
+      );
+      return item;
+    });
+
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col md={7}>
+              <div className={classes.root}>{cliqueMembers}</div>
+            </Col>
+            <Col md={5}>
+              {this.state.text && (
+                <div>
+                  <Typography className={classes.text} variant="h5">
+                    {this.state.text}
+                  </Typography>
+                  <Divider />
+                  <Typography className={classes.text} variant="body2">
+                    {this.state.text}
+                  </Typography>
+                </div>
+              )}
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(CircleView);
--- clients/app/src/components/dashboards/ViewGroup.js
+++ clients/app/src/components/dashboards/ViewGroup.js
@@ -1,36 +1,37 @@
-import React, { Component } from "react";
-import { withStyles } from "@material-ui/core/styles";
-import SimpleCard from "../SimpleCard";
-import { Container, Row, Col } from "react-grid-system";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import Feed from "./Feed";
-import MembersTable from "./Members";
-import MakePayment from "../payment/MakePayment";
-import GroupDetais from "./GroupDetais";
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import SimpleCard from '../SimpleCard';
+import { Container, Row, Col } from 'react-grid-system';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Feed from './Feed';
+import MembersTable from './Members';
+import MakePayment from '../payment/MakePayment';
+import GroupDetais from './GroupDetais';
+import CircleView from '../circles/Circles';
 
 const feeds = [
   {
-    title: "Requesting Early Payment",
-    description: "I need to get paid this month for...",
-    time: "25 min ago",
-    author: "John Smith",
-    authorImg: "https://v3-0-0.material-ui.com/static/images/remy.jpg"
+    title: 'Requesting Early Payment',
+    description: 'I need to get paid this month for...',
+    time: '25 min ago',
+    author: 'John Smith',
+    authorImg: 'https://v3-0-0.material-ui.com/static/images/remy.jpg'
   },
   {
-    title: "Requesting Early Payment",
-    description: "I need to get paid this month for...",
-    time: "50 min ago",
-    author: "Sam Lou",
-    authorImg: "https://v3-0-0.material-ui.com/static/images/uxceo-128.jpg"
+    title: 'Requesting Early Payment',
+    description: 'I need to get paid this month for...',
+    time: '50 min ago',
+    author: 'Sam Lou',
+    authorImg: 'https://v3-0-0.material-ui.com/static/images/uxceo-128.jpg'
   }
 ];
 const styles = theme => ({
   btnContainer: {
-    display: "flex"
+    display: 'flex'
   },
   button: {
-    marginLeft: "auto",
+    marginLeft: 'auto',
     marginTop: 10
   }
 });
@@ -39,7 +40,7 @@ class ViewGroup extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      plea: ""
+      plea: ''
     };
   }
 
@@ -50,9 +51,9 @@ class ViewGroup extends Component {
   };
   handleSubmit = e => {
     e.preventDefault();
-    console.log("your plea " + this.state.plea + " was submitted");
+    console.log('your plea ' + this.state.plea + ' was submitted');
     this.setState({
-      plea: ""
+      plea: ''
     });
   };
   render() {
@@ -62,8 +63,15 @@ class ViewGroup extends Component {
         <Container style={{ marginTop: 10 }}>
           <Row>
             <Col>
-              <SimpleCard title="Make Payment" style={{ display: "flex" }}>
+              <SimpleCard title="Make Payment" style={{ display: 'flex' }}>
                 <MakePayment />
+              </SimpleCard>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <SimpleCard title="Your Clique" style={{ display: 'flex' }}>
+                <CircleView />
               </SimpleCard>
             </Col>
           </Row>
